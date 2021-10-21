1dd2fca668b56c3e08cbd8683af49e6ed64ea469
AndresRobayo <luwin.robayo@gmail.com>
2021-10-21 18:19:23 -0500

    Plantilla Andres Robayo

---
+++ Sprint4/desarrollo_proyecto/Funcional renta vehiculo - Andres Robayo/capturadatos.php
@@ -0,0 +1,14 @@
+<?php
+$nombre = $_POST['nombre'];
+$documento = $_POST['documento'];
+$tipo_documento = $_POST['tipo_documento'];
+$email = $_POST['email'];
+$Fecha_Inicio = $_POST['Fecha_Inicio'];
+$Fecha_Final = $_POST['Fecha_Final'];
+echo $nombre;
+echo $documento;
+echo $tipo_documento;
+echo $email;
+echo $Fecha_Inicio;
+echo $Fecha_Final;
+?>
